7eecb729c41ad3ba6ebe7777658cf4f739a1d074
Hannes Eilers <mail@hanneseilers.de>
2020-08-12 13:24:45 +0200

    Updated welcome message and explanations

--- main.py
+++ main.py
@@ -108,9 +108,7 @@ class Main:
 		self.outputDir = outputDir
 		self.inputDir = inputDir
 
-	def start(self):
-		logging.info( "Welcome to key generator" )
-		
+	def start(self):		
 		if self.filesystem.check( self.inputDir, self.outputDir, self.inputFile, self.usedKeysFile ):
 			self.keys = self.filesystem.readKeys( self.inputDir + "/" + self.usedKeysFile )
 			self.n_userdata, self.userdata = self.filesystem.readUsers( self.inputDir + "/" + self.inputFile )
@@ -151,17 +149,70 @@ class Main:
 			html = "<html>\n<head>\n<title>{title}</title>\n<style>\n{css}\n</style>\n</head>".format( title = group, css = "@page { size: 21cm 29.7cm; margin: 15mm 15mm 15mm 15mm;} table, th, td {border: 1px solid black; border-collapse: collapse; padding: 5pt;} body{font-family: Arial, sans-serif; font-size: 16pt;" )
 			html += "<body>\n<h1>Klasse: {group}</h1>\n<table style='width: 100%; border: 1px;'>\n<thead>\n<tr>\n<th><b>Nachname</b></th><th><b>Vorname</b></th><th><b>Schl&uuml;ssel</b></th>\n</tr>\n</thead><tbody>".format( group=group)
 			for uid, user in sorted( self.userdata[group].items(), key=lambda u: u[1]['name'] ):
-				logging.info(user)
 				html += "\n<tr>\n<td>{name}</td><td>{prename}</td><td>{key}</td>\n</tr>".format( name=user['name'], prename=user['prename'], key=user['key'] )
 			
 			html += "\n</tbody>\n</table>\n</body>\n</html>"
 			f.write(html)
 			f.close()
+			
+	def welcome(self):
+		ret = False		
+		print( "-------- Welcome to KEY GENERATOR --------" )
+		print( "This program can generate unique keys for a list of users.")
+		print( "The program needs: A list of users as .csv format following this layout:" ) 
+		print( "\n\tID,LAST NAME, PRENAME, GROUP\n" )
+		print( "hint: you can put the names and group into \" brackets for showing this is one string. Like 1, \"Jon Maria Jack\", \"Doe\"" )
+		
+		# excel tutorial
+		if self.ask( "Do you need help converting your excel sheet into .csv format?", ["y", "n"] ) == "y":
+			print( "1. Remove all leading and tailing rows. Also the ones including your table description (like nr, name, prename, group, ...)." )
+			print( " There should only be the columns with users data." )
+			print( "2. Select File > Save as > select comma seperated values (.csv) on the bottom right of opening window inside file format selector." )
+			print( "3. Save your file as input.csv\n" )
+			
+		print( "Upload your input.csv file with input users data and if you have keys.txt to directory " + self.inputDir + "." )
+		
+		# upload tutorial
+		if self.ask( "Do you need help with uploading?", ["y", "n"] ) == "y":
+			print( "- You can simply drag and drop you files from your directory on pc inside your browser. Release them above " + self.inputDir + " directory." )
+			print( "- Otherwise you can click the three dots above files list and select Upload File." )
+			print( " Upload your file. It will appear outside " + self.inputDir + " directory. Use drag and rop o move it here." )
+			
+		# continue
+		if self.ask( "Do you uploaded as input.csv file, correctly formatted (like emaple above) and optional a keys.txt into " + self.inputDir + "?", ["y", "n"] ) == "y":
+			print( "Fine! That's all. The program will continue shortly.\n" )
+			print( "The users will get extracted from your input.csv file and unique, crypotraphic safe keys will be assigned to them.\n" )
+			print( "The program will generate output file in " + self.outputDir + " directory." )
+			print( "For every group in your users data, there will be a .html file with a list of which user has which key." )
+			print( "You can normally open them with double click inside your web browser and directly print them." )
+			print( "The program will also generate a keys.txt file, containing all the generated keys." )
+			print( "You can use this keys.txt inside " + self.inputDir + " directory next time you use this program." )
+			print( "The program than will not generate new keys, that are the same as the old keys.\n" )
+			print( "DOWNLOAD DATA: After program finished, you can download all data:" )
+			print( "1. Click the three dots above files list and select Download as zip" )
+			print( "2. Save zip on your computer and unzip it." )
+			print( "3. Take a look inside the unzipped content. You find the whole program, with generated output here!" )
+			ret = True
+
+		print( "------------------------------------------" )
+		return ret
+		
+	def ask(self, msg="", choices=[]):
+		selected = ""
+		while True:
+			print( "\n\t# " + msg)
+			selected = input( "\t" + str(choices) + ": " )
+			if selected in choices:
+				break
+		
+		print("")
+		return selected
 		
 if __name__ == "__main__":
-	logging.basicConfig(level=logging.INFO)
-	
+	logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+
 	main = Main( inputDir, outputDir, inputFile, usedKeysFile )
-	main.start()
+	if main.welcome():
+		main.start()
 	
 	
